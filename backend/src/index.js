0df11fbe2dddcd121ea240298e22f31ca57b0288
morpheusblack <leonardord99@gmail.com>
2020-01-22 18:47:07 -0300

    Frontend added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://Leonardo:omnistack@cluster0-wynb1.mongodb.net/devMaps?retryWrites=true&w=majority", {
+mongoose.connect(
+  "mongodb+srv://Leonardo:omnistack@cluster0-wynb1.mongodb.net/devMaps?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
